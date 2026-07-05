fe5c19406f440c5b56b92c57e7ce816414fb2a2a
srishcodes <srishtisaini4444@gmail.com>
2026-07-06 03:44:24 +0530

    Add root API route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("⚔️ Welcome to Dungeon Quest API!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
