c655df46acc9f47314a45a3b57c49679d3b52406
srishcodes <srishtisaini4444@gmail.com>
2026-07-07 00:25:39 +0530

    Connect fight button to backend

--- public/script.js
+++ public/script.js
@@ -12,3 +12,30 @@ async function loadPlayer(){
 }
 
 loadPlayer();
+
+async function fight(){
+
+    const response = await fetch("/fight",{
+        method:"POST"
+    });
+
+    const result = await response.json();
+
+    document.getElementById("hp").textContent = result.player.hp;
+    document.getElementById("level").textContent = result.player.level;
+    document.getElementById("gold").textContent = result.player.gold;
+    document.getElementById("weapon").textContent = result.player.weapon;
+
+    document.getElementById("battleLog").innerHTML =
+    `
+    ⚔️ You fought a ${result.enemy}!<br>
+    ❤️ Damage: ${result.damageTaken}<br>
+    ⭐ XP: +${result.xpGained}<br>
+    💰 Gold: +${result.goldGained}<br>
+    🎒 Loot: ${result.loot}
+    `;
+}
+
+document
+.getElementById("fightBtn")
+.addEventListener("click", fight);
